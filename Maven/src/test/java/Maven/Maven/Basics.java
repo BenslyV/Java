e9d1b8195d565899e2cbe877026eb0d42ee4c932
BenslyV <bensly.v@gmail.com>
2019-06-13 18:14:38 +0530

    checking office

---
+++ Maven/src/test/java/Maven/Maven/Basics.java
@@ -0,0 +1,13 @@
+package Maven.Maven;
+
+public class Basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello maven");
+		
+	
+
+	}
+
+}
---
+++ Maven/src/test/java/Maven/Maven/MavenTestNG.java
@@ -0,0 +1,13 @@
+package Maven.Maven;
+
+import org.testng.annotations.Test;
+
+public class MavenTestNG {
+	@Test
+	public void test()
+	{
+		System.out.println("hello Maven");
+	
+	}
+
+}
